873c2a185a80d89019a6cd4df599b46e489b11ee
Khushnud Eshtemirov <115606523+KhushnudEshtemirov@users.noreply.github.com>
2024-11-14 16:56:26 +0500

    Update FirstApplication.java

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -2,6 +2,6 @@ package com.mjc.stage0;
 
 public class FirstApplication {
     public static void main(String[] args) {
-        System.out.printf("Hello, World!");
+        System.out.println("Hello, World!");
     }
 }
